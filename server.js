fd000f856632b584d887e0c60f166c7dd9295769
Vinny <vinnytorde@gmail.com>
2018-09-17 21:17:35 -0500

    first express test successful

--- server.js
+++ server.js
@@ -1,1 +1,8 @@
 const express = require('express')
+
+const app = express()
+app.get('/test', function(request, response) {
+  response.send({ someFancyString: 'Hello World!' })
+})
+
+app.listen(process.env.PORT || 3000)
