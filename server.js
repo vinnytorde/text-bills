aa01ee42f448f4b79a5bb02df18a05b086ae5545
Vinny <vinnytorde@gmail.com>
2018-09-18 14:44:26 -0500

    structure: body parser + basic GET and POST

--- server.js
+++ server.js
@@ -1,11 +1,14 @@
 const express = require('express')
 const morgan = require('morgan')
 const mongo = require('./drivers/mongo')
-
+const bodyParser = require('body-parser')
 const app = express()
 
-//logger
+//logger middleware
 app.use(morgan('tiny'))
+//parser middleware
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }))
 
 app.get('/test', function(request, response) {
   mongo().then(col => {
@@ -13,6 +16,17 @@ app.get('/test', function(request, response) {
       response.send({ length: data.length })
     })
   })
+})
+
+app.post('/bill', function(request, response) {
+  console.log(request.body)
+  response.send({ test: 'you done been posted' })
+})
+
+app.get('/report/:user', function(request, response) {
+  console.log(request.params.user)
+  const { user } = request.params
+  response.send('you are reporting for ' + user)
 })
 
 module.exports = app
