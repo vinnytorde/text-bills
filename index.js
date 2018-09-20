5ad8297cfe597cd512ceac2f1d41655b6222a97f
Vinny <vinnytorde@gmail.com>
2018-09-20 15:02:46 -0500

    pre-heroku

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+console.clear()
+
 // environment variables
 const environment = require('dotenv')
 environment.config()
@@ -5,5 +7,5 @@ environment.config()
 // express server
 const server = require('./server.js')
 const port = process.env.PORT || 3000
-const success = (a, b, c, d) => console.log(`Serving from port ${port}`)
+const success = () => console.log(`Serving from port ${port}`)
 server.listen(port, success)
--- server.js
+++ server.js
@@ -22,17 +22,25 @@ app.get('/test', function(request, response) {
 
 app.post('/bill', function(request, response) {
   const { name } = request.body
-  const vinny = 2102195643
-  const to = vinny
   const body = `${name} has paid a bill!`
-  twilio(to, body).then(message => {
-    response.send({ vinny, body, name })
+  twilio(body, 2102195643).then(message => {
+    message.sid
   })
 })
 
 app.get('/report/:user', function(request, response) {
   const { user } = request.params
   response.send('you are reporting for ' + user)
+})
+
+app.post('/register', function(request, response) {
+  const input = request.body
+  console.log(input)
+  console.log(request)
+  response
+    .status(200)
+    .json({ input })
+    .send()
 })
 
 module.exports = app
