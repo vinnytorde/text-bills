f74a777335d50f024b85b2f8452f8d6d7949fb46
Vinny <vinnytorde@gmail.com>
2018-09-20 15:38:06 -0500

    bugfix on error tracking

--- drivers/mongo.js
+++ drivers/mongo.js
@@ -14,7 +14,7 @@ function driver(col = DB_COL_BILLS) {
       url,
       { useNewUrlParser: true },
       function(err, client) {
-        if (err) throw error
+        if (err) throw err
         const collection = client.db(DB_NAME).collection(col)
         // BSON != JSON
         resolve(collection)
